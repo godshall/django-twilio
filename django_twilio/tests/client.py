7f471da2fe994ed3bb12971c509a5a7594738927
Randall Degges <rdegges@gmail.com>
2012-04-14 15:47:28 -0700

    Renaming some tests for clarity.
    Also: fixing our import path to be more standardized.

--- django_twilio/tests/client.py
+++ django_twilio/tests/client.py
@@ -1,15 +1,18 @@
+from django.conf import settings
 from django.test import TestCase
 from twilio.rest import TwilioRestClient
 
 from django_twilio.client import twilio_client
-from django_twilio.settings import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
 
 
 class TwilioClientTestCase(TestCase):
 
-    def test_twilio_client_exists(self):
+    def test_exists(self):
         self.assertIsInstance(twilio_client, TwilioRestClient)
 
-    def test_twilio_client_sets_creds(self):
-        self.assertEqual(twilio_client.auth, (TWILIO_ACCOUNT_SID,
-                TWILIO_AUTH_TOKEN))
+    def test_sets_creds(self):
+        self.assertEqual(twilio_client.auth, (settings.TWILIO_ACCOUNT_SID,
+                settings.TWILIO_AUTH_TOKEN))
+
+#    def test_sets_api_version_to_default(self):
+#        self.assertIn(settings.TWILIO_API_VERSION, twilio_client.account_uri)
